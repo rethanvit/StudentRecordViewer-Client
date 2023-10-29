49b680dd4d4decc9f1df8d8b37563ad9c500cc89
rbputt <rethan.putta@insperity.com>
2023-10-29 18:27:47 -0500

    add unit test for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { AuthService } from './shared/Auth/auth.service';
 
 describe('AppComponent', () => {
+
+  let mockAuthService;
+
   beforeEach(async () => {
+    mockAuthService = jasmine.createSpyObj(["AutoLogin"]);
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService }
+      ]
     }).compileComponents();
   });
 
@@ -16,16 +25,23 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SRV_Client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('SRV_Client');
-  });
+  //it(`should have as title 'SRV_Client'`, () => {
+  //  const fixture = TestBed.createComponent(AppComponent);
+  //  const app = fixture.componentInstance;
+  //  expect(app.title).toEqual('SRV_Client');
+  //});
 
-  it('should render title', () => {
+  //it('should render title', () => {
+  //  const fixture = TestBed.createComponent(AppComponent);
+  //  fixture.detectChanges();
+  //  const compiled = fixture.nativeElement as HTMLElement;
+  //  expect(compiled.querySelector('.content span')?.textContent).toContain('SRV_Client app is running!');
+  //});
+
+  it('OnInit should call AutoLogin from AuthService', async () => {
     const fixture = TestBed.createComponent(AppComponent);
+    
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SRV_Client app is running!');
+    expect(mockAuthService.AutoLogin).toHaveBeenCalled();
   });
 });
